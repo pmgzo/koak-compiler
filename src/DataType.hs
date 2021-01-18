1d7994d9853f3e47d6ead849a3859715adeced67
gonzalo <pierremickael.gonzalo@gmail.com>
2021-01-18 18:22:15 +0100

    [Add]:Datatype in src

--- src/DataType.hs
+++ src/DataType.hs
@@ -5,6 +5,7 @@
 data Identifier = Wait String | Typed String TypeKoak deriving (Show, Eq)
 
 data Unop = Not | UMinus deriving (Show, Eq)
+
 data Binop = Time | Div | Plus | Minus | Lt | Gt | Eq | NotEq | Assign deriving (Show, Eq)
 
 data CallExpr = CallExpr Identifier [Expr] deriving (Show, Eq)
@@ -18,25 +19,23 @@ data Postfix = C CallExpr | L Lit | Id Identifier | E Exprs deriving (Show, Eq) 
 data Unary = UNOP Unop Unary | P Postfix deriving (Show, Eq)
 
 -- data Expr = U Unary | OP1 Unary [(Binop, Unary)] | OP2 Unary [(Binop, Expr)]
-data Expr = OP1 Unary [(Binop, Expr)] deriving (Show, Eq)
 
-data Exprs = Exprs [Expr] deriving (Show, Eq)-- ++ For, if, else, while
+-- 5 + 5 + 5
+data Expr = U Unary | OP1 Unary [(Binop, Expr)] deriving (Show, Eq)
 
--- data While = (, , , Expr)
-data For = For (Identifier, Expr) (Identifier, Expr) Expr Exprs deriving (Show, Eq) -- init i, cmp, it++, block
-
-data While = While Expr Exprs deriving (Show, Eq)
-
-data If = IfThen Expr Exprs | IfElse Expr Exprs Exprs deriving (Show, Eq)
+-- 6 + 5 : 5 + 7
+data Exprs = Exprs [Expr] 
+            | For (Identifier, Expr) (Identifier, Expr) Expr Exprs
+            | While Expr Exprs
+            | IfThen Expr Exprs 
+            | IfElse Expr Exprs Exprs deriving (Show, Eq)
 
 data TypeKoak = INT | DOUBLE | VOID deriving (Show, Eq)
 
-type Args = [Identifier] 
+type Args = [Identifier]
 
 data Proto = Proto Identifier Args deriving (Show, Eq)-- ++ Unary, Binary
 
-data Def = Def Proto Expr deriving (Show, Eq)
-
-data KDef = Df Def | Expr Exprs deriving (Show, Eq)
+data KDef = Def Proto Expr  | Xpr Exprs deriving (Show, Eq)
 
 type AST = [KDef]
