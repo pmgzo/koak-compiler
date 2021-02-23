a2fc074d0925b84908b6d22bf65df43edb613665
gonzalo <pierremickael.gonzalo@gmail.com>
2021-02-23 15:16:42 +0100

    [Add]: error management in main

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,15 @@
--- import qualified LLVM.AST as AST
--- import qualified LLVM.AST.Constant as C
--- import qualified LLVM.AST.Float as F
--- import LLVM.AST
-
+import System.Exit (exitSuccess, exitWith)
+import System.Exit
 import LLVM_Module
 import TypeInference
+import DataType2
 
 main :: IO ()
 main = do
-  -- genObjFromExpr $ inferringType $ functionPaul
-  
+  let exprs = inferringType []--functionPaul
+  case exprs of
+    [(Err str)] -> exitWith (ExitFailure 84)
+    []  -> print("empty array") >> exitWith (ExitFailure 84)
+    exprs2 -> genObjFromExpr "obj" exprs2
+
   print("here")
