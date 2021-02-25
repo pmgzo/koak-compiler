8913eb25bab42e0b30aa969d4d116e96056dca41
aurele <aurele.auboin@epitech.eu>
2021-02-25 10:54:34 +0100

    [Edit]: test in koak to match main

--- tests/test.sh
+++ tests/test.sh
@@ -5,15 +5,16 @@ declare -a arr=( "exampleKoak/test_fib_iterative.kk" "exampleKoak/test.kk" "exam
 
 
 
-mv tests/exec_test.sh .
+mv tests/test.sh .
 #######################################################
 make re
 
 for arg in "${arr[@]}"; do
     rm -f *.o
     echo -e "\033[1;35m"./koak $arg"\033[0m"
-    ./koak $arg && gcc main.c exampleKoak/*.o && ./a.out
+    ./koak $arg && gcc tests/main.c exampleKoak/*.o && ./a.out || echo -e "\033[1;91m"FAILED"\033[0m"
+
     # ./exec_test.sh $arg
 done
 #######################################################
-mv exec_test.sh tests
+mv test.sh tests
