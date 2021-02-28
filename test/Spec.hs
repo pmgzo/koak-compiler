79e9d50f35c761c1e17be8447b8b6aa50e4a8348
aurele <aurele.auboin@epitech.eu>
2021-02-28 09:36:28 +0100

    [Fix]: error handling in typeInference

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,6 @@ import Test.HUnit
 import TestLLVMBuilder
 import TypeInferenceTest
 
--- resTest11ParserList  = Just ((Value T EndList), " ")
--- test11ParserList     = TestCase $ assertEqual "(#t ) " resTest11ParserList (parseList "(#t ) ")
-
 main = do
     runTestTT llvmBuilderTests
     runTestTT typeInferenceTests
