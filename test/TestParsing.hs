da3b47d03d2d036acc5b69f2b8cc2ddad3ac7888
Paul <paul.cochet@epitech.eu>
2021-03-01 10:55:02 +0100

    [FIX] parseword

--- test/TestParsing.hs
+++ test/TestParsing.hs
@@ -14,4 +14,13 @@ import DataType2
 expectedRes1 = Just ("table", "top")
 test1 = TestCase $ assertEqual "word table" expectedRes1 (runParser (word "table") "tabletop")
 
-parsingTests = TestList [test1]
+expectedRes2 = Just ("id", ":int):int;")
+test2 = TestCase $ assertEqual "word table" expectedRes2 (runParser (word "id") "id:int):int;")
+
+expectedRes3 = Just ("func", "(x+2)")
+test3 = TestCase $ assertEqual "word table" expectedRes3 (runParser (word "func") "func(x+2)")
+
+expectedRes4 = Just ("word", " from a sentence")
+test4 = TestCase $ assertEqual "word table" expectedRes4 (runParser (word "word") "  word from a sentence")
+
+parsingTests = TestList [test1, test2, test3, test4]
