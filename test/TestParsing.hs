e31165d464f0dc701fce00f642eb3576ddbe5da5
Paul <paul.cochet@epitech.eu>
2021-03-01 10:59:13 +0100

    [ADD] test char

--- test/TestParsing.hs
+++ test/TestParsing.hs
@@ -12,15 +12,18 @@ import Parse
 import DataType2
 
 expectedRes1 = Just ("table", "top")
-test1 = TestCase $ assertEqual "word table" expectedRes1 (runParser (word "table") "tabletop")
+word1 = TestCase $ assertEqual "word table" expectedRes1 (runParser (word "table") "tabletop")
 
 expectedRes2 = Just ("id", ":int):int;")
-test2 = TestCase $ assertEqual "word table" expectedRes2 (runParser (word "id") "id:int):int;")
+word2 = TestCase $ assertEqual "word table" expectedRes2 (runParser (word "id") "id:int):int;")
 
 expectedRes3 = Just ("func", "(x+2)")
-test3 = TestCase $ assertEqual "word table" expectedRes3 (runParser (word "func") "func(x+2)")
+word3 = TestCase $ assertEqual "word table" expectedRes3 (runParser (word "func") "func(x+2)")
 
 expectedRes4 = Just ("word", " from a sentence")
-test4 = TestCase $ assertEqual "word table" expectedRes4 (runParser (word "word") "  word from a sentence")
+word4 = TestCase $ assertEqual "word table" expectedRes4 (runParser (word "word") "  word from a sentence")
 
-parsingTests = TestList [test1, test2, test3, test4]
+expectedRes5 = Just ('t', "abletop")
+char1 = TestCase $ assertEqual "char t abletop" expectedRes5 (runParser (char 't') "tabletop")
+
+parsingTests = TestList [word1, word2, word3, word4, char1]
