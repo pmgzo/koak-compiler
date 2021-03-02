63ea7941dc1caba3432863e2a3c331babd2fe386
gonzalo <pierremickael.gonzalo@gmail.com>
2021-03-02 12:40:24 +0100

    [Update]: variable in tests

--- tests/main.c
+++ tests/main.c
@@ -12,34 +12,34 @@ double dtest3();
 int itest4();
 double dtest4();
 
-int iexpected_res1;
-int iexpected_res2;
-int iexpected_res3;
-int iexpected_res4;
-double dexpected_res1;
-double dexpected_res2;
-double dexpected_res3;
-double dexpected_res4;
+extern int iexpectedRes1;
+extern int iexpectedRes2;
+extern int iexpectedRes3;
+extern int iexpectedRes4;
+extern double dexpectedRes1;
+extern double dexpectedRes2;
+extern double dexpectedRes3;
+extern double dexpectedRes4;
 
 extern int returnType; //0 == int, 1 == double
 
-int iprintTest(char *test_name, int res, int expected_res)
+int iprintTest(char *test_name, int res, int expectedRes)
 {
-    if (res == expected_res) {
+    if (res == expectedRes) {
         printf("\033[1;32mSuccess %s\n\033[0m", test_name);
     } else {
-        printf("\033[1;31mFailed %s\033[0m\n expected this = %d, receive this %d\n", test_name, expected_res, res);
+        printf("\033[1;31mFailed %s\033[0m\n expected this = %d, receive this %d\n", test_name, expectedRes, res);
         return (84);
     }
     return (0);
 }
 
-int dprintTest(char *test_name, double res, double expected_res)
+int dprintTest(char *test_name, double res, double expectedRes)
 {
-    if (res == expected_res) {
+    if (res == expectedRes) {
         printf("\033[1;32mSuccess %s\n\033[0m", test_name);
     } else {
-        printf("\033[1;31mFailed %s\033[0m\n expected this = %f, received this %f\n", test_name, expected_res, res);
+        printf("\033[1;31mFailed %s\033[0m\n expected this = %f, received this %f\n", test_name, expectedRes, res);
     }
     return (0);
 }
@@ -47,16 +47,16 @@ int dprintTest(char *test_name, double res, double expected_res)
 int main(int ac, char **av, char **env)
 {
     if (returnType == 0) {
-        iprintTest(av[1], itest1(), iexpected_res1);
-        iprintTest(av[1], itest2(), iexpected_res2);
-        iprintTest(av[1], itest3(), iexpected_res3);
-        iprintTest(av[1], itest4(), iexpected_res4);
+        iprintTest(av[1], itest1(), iexpectedRes1);
+        iprintTest(av[1], itest2(), iexpectedRes2);
+        iprintTest(av[1], itest3(), iexpectedRes3);
+        iprintTest(av[1], itest4(), iexpectedRes4);
     }
     else if (returnType == 1) {
-        dprintTest(av[1], itest1(), dexpected_res1);
-        dprintTest(av[1], itest2(), dexpected_res2);
-        dprintTest(av[1], itest3(), dexpected_res3);
-        dprintTest(av[1], itest4(), dexpected_res4);
+        dprintTest(av[1], itest1(), dexpectedRes1);
+        dprintTest(av[1], itest2(), dexpectedRes2);
+        dprintTest(av[1], itest3(), dexpectedRes3);
+        dprintTest(av[1], itest4(), dexpectedRes4);
     }
     return (0);
 }
